610817c81009a6b55bcfe6cbb1e27768e428a9ee
StormVRguy <43267887+StormVRguy@users.noreply.github.com>
2020-02-01 17:05:14 +0100

    UI e menu vari
    UI e menu vari

---
+++ Assets/PauseSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseSystem : MonoBehaviour
+{
+	
+	public GameObject pauseMenu;
+	public bool pause = false;
+	
+    void Start()
+    {
+        pauseMenu=GameObject.Find("Pause Menu");
+		PauseGame(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+			pause = !pause;
+			PauseGame(pause);
+		}
+    }
+	
+	public void PauseGame(bool newState) {
+		pause = newState;
+		if(pause) {
+			Time.timeScale = 0f;
+			pauseMenu.SetActive(true);
+		}
+		else {
+			Time.timeScale = 1f;
+			pauseMenu.SetActive(false);
+		}
+	}
+}
