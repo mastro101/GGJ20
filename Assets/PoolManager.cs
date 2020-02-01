d489c8e9cfe42a0947d2fba3502540230185ed27
Gabriele Stramandino <gabriele.stramandino@gmail.com>
2020-02-01 10:06:20 +0100

    Beta

---
+++ Assets/PoolManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class ObjectPoolItem
+    {
+        public int amountToPool;
+        public GameObject objectToPool;
+        public bool shouldExpand;
+    }
+
+    public static PoolManager SharedInstance;
+
+    private List<GameObject> pooledObjects;
+    public ObjectPoolItem[] itemsToPool;
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+
+        foreach (ObjectPoolItem item in itemsToPool)
+        {
+            for (int i = 0; i < item.amountToPool; i++)
+            {
+                var poolItem = (GameObject)Instantiate(item.objectToPool);
+                poolItem.transform.parent=transform;
+                poolItem.SetActive(false);
+                pooledObjects.Add(poolItem);
+            }
+        }
+    }
+
+    public GameObject GetPooledObject(string tag)
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy && pooledObjects[i].tag == tag)
+            {
+                return pooledObjects[i];
+            }
+        }
+        foreach (ObjectPoolItem item in itemsToPool)
+        {
+            if (item.objectToPool.tag == tag)
+            {
+                if (item.shouldExpand)
+                {
+                    var poolItem = (GameObject)Instantiate(item.objectToPool);
+                    poolItem.transform.parent=transform;
+                    poolItem.SetActive(false);
+                    pooledObjects.Add(poolItem);
+                    return poolItem;
+                }
+            }
+        }
+        return null;
+    }
+
+    // private void ResetObject(GameObject go){
+    //     go.transform.position = go.originalPosition.position;
+    //     go.transform.rotation = go.originalPosition.rotation;
+    // }
+}
---
+++ Assets/SwordMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordMovement : MonoBehaviour
+{
+    public int controllerInput;
+
+    private bool isMoving;
+    Vector2 direction;
+    float startingPos;
+
+    public float range;
+    public float steps;
+
+    private float maxRangeLeft;
+    private float maxRangeRight;
+
+    public float timeBeforeNextMovement=0;
+    private Rigidbody2D rb2D;
+
+    void Start()
+    {
+        startingPos=transform.position.x;
+        maxRangeLeft=(startingPos-range/2);
+        maxRangeRight=(startingPos+range/2);
+        rb2D=GetComponent<Rigidbody2D>();
+    }
+  
+    void Update()
+    {
+        Vector2 stickDirection;
+        if (controllerInput != 0)
+        {
+            stickDirection = new Vector2(Input.GetAxis("Horizontal" + controllerInput.ToString()), Input.GetAxis("Vertical" + controllerInput.ToString()));
+        }
+        else
+        {
+            stickDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+
+        HandleMovement(stickDirection.normalized);
+    }
+
+    void HandleMovement(Vector2 stickDirection){
+        if(stickDirection.x!=0 && !isMoving){
+            isMoving=true;
+
+            var currentPos=Mathf.Abs(transform.position.x);
+            if(currentPos>=maxRangeLeft && currentPos<=maxRangeRight){
+                if(stickDirection.x>0 && (currentPos+(range/steps))<maxRangeRight)
+                    MovementHandler((range/steps));
+
+                if(stickDirection.x<0 && (currentPos-(range/steps))>maxRangeLeft)
+                    MovementHandler(-(range/steps));
+            }
+            StartCoroutine(AllowNextMovement());
+        }
+    }
+
+    private IEnumerator AllowNextMovement(){
+        yield return new WaitForSeconds(timeBeforeNextMovement);  
+        isMoving=false;
+    }
+
+    void MovementHandler(float moveAmount)
+    {
+        Vector2 xMovement=new Vector2(moveAmount,0);
+        rb2D.MovePosition(Vector2Utility.ConvertV3InV2(transform.position) + xMovement);
+    }   
+    
+}
