2b5b0dc53f6d8cf244e2d5a488c152c716f8eb17
EasterMaster <teo.mastropasqua@gmail.com>
2020-02-01 14:32:56 +0100

    Implementati Clienti

---
+++ Assets/_GGJ 2020/Customers/Scripts/CustomersQueue.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomersQueue : MonoBehaviour
+{
+    [SerializeField] Transform CustomerBox;
+    [SerializeField] int DifferentCustomers;
+    [SerializeField] Transform swordSpawnPoint;
+
+    List<Customers> customers = new List<Customers>();
+
+    public System.Action<Customers> onAddCustomers;
+
+    int oldCustomerDifficult = -1;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            AddCustomer();
+            SpawnSword();
+        }
+    }
+
+    public void AddCustomer()
+    {
+        Customers _customer = null;
+        int randomInt = 1;
+        for (int i = 0; i < 10; i++)
+        {
+            randomInt = Random.Range(1, DifferentCustomers + 1);
+            _customer = PoolManager.SharedInstance.GetPooledObjectWithoutInstantiate("Customer" + randomInt).GetComponent<Customers>();
+            if (oldCustomerDifficult == _customer.difficult)
+            {
+                continue;
+            }
+            else
+            {
+                _customer = PoolManager.SharedInstance.GetPooledObject("Customer" + randomInt).GetComponent<Customers>();
+                break;
+            }
+
+        }
+        _customer.transform.SetParent(CustomerBox);
+        onAddCustomers?.Invoke(_customer.GetComponent<Customers>());
+        oldCustomerDifficult = randomInt;
+        customers.Add(_customer);
+    }
+
+    public Customers GetCustomers()
+    {
+        return customers[0];
+    }
+
+    public SwordMovement SpawnSword()
+    {
+        return customers[0].InstantiateSword(swordSpawnPoint.position);
+    }
+
+    public Customers NextCustomer()
+    {
+        GameObject _customer = customers[0].gameObject;
+        customers.Remove(customers[0]);
+        Destroy(_customer);
+        return customers[0];
+    }
+}
