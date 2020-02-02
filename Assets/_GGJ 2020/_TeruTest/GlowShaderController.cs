4d63c2a8de69607b42484b602a66efacde18a8e2
AndreaAncona <a.ancona.bb@gmail.com>
2020-02-02 12:49:19 +0100

    Aggiunto Glow Oggetti

---
+++ Assets/_GGJ 2020/_TeruTest/GlowShaderController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlowShaderController : MonoBehaviour
+{
+    public AnimationCurve animationCurve;
+	private float timer;
+    public float animationDuration = 1;
+	public Material material;
+
+    public void Awake()
+	{
+		material = GetComponent < Image > ().material;
+	}
+
+    public void StartAnimation()
+    {
+		timer = 0;
+    }
+
+    public void Update()
+    {
+		timer += Time.deltaTime;
+		float evaluationParam = timer / animationDuration;
+		Debug.Log(animationCurve.Evaluate(evaluationParam));
+		material.SetFloat("_LerpValue", animationCurve.Evaluate(evaluationParam));
+    }
+
+}
